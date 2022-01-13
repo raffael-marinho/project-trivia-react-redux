a79674371ba5b0d0fa9667235dc0e3068c8179dc
adrancarnavale <adran.carnavale@gmail.com>
2022-01-13 18:23:18 -0300

    finaliza requisito 01

---
+++ src/components/Content.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from '../pages/Login';
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" render={ () => <Login /> } />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Content;
