4ea6793e8388b2ca6090fb8155e62ad1177a4fe0
raffael marinho <silvamarinho10@gmail.com>
2022-01-14 18:28:50 -0300

    ajuda no requisito 4

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import md5 from 'crypto-js/md5';
 
 class Game extends Component {
   render() {
+    const { nome, email } = this.props;
+
+    function gravatar() {
+      return `https://www.gravatar.com/avatar/${md5(email).toString()} `;
+    }
+
     return (
       <div>
         <header>
-          <img src="" alt="" data-testid="header-profile-picture" />
-          <h2 data-testid="header-player-name">nome da pessoa</h2>
+          <img src={ gravatar } alt="" data-testid="header-profile-picture" />
+          <h2 data-testid="header-player-name">{nome}</h2>
           <div data-testid="header-score">placar</div>
         </header>
       </div>
@@ -14,4 +23,16 @@ class Game extends Component {
   }
 }
 
-export default Game;
+function mapStateToProps(state) {
+  return {
+    nome: state.userReducer.nome,
+    email: state.userReducer.email,
+  };
+}
+
+Game.propTypes = {
+  nome: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Game);
